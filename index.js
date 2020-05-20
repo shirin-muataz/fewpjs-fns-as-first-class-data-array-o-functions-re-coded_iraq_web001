642da5e3e1446f178589821b4e7362879a78d6b2
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-20 09:30:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function wakeDog(dogName, dogBreed) {
+ const st=`Wake ${dogName} the ${dogBreed}`;
+ return(st);
+}
+
+function leashDog(dogName, dogBreed) {
+  const st =`Leash ${dogName} the ${dogBreed}`;
+  return(st);
+}
+
+function walkToPark(dogName, dogBreed) {
+  const st=`Walk to the park with ${dogName} the ${dogBreed}`;
+  return(st);
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  const st=`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  return(st);
+}
+
+function walkHome(dogName, dogBreed) {
+  const st=`Walk home with ${dogName} the ${dogBreed}`;
+  return(st);
+}
+
+function unleashDog(dogName, dogBreed) {
+  const st=`Unleash ${dogName} the ${dogBreed}`;
+  return(st);
+}
+
+const routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
+//const dogName=["Xerox","Boo Radley","Bunny","Mary J.Blige","Santa Paws","Lauren","Skippy"];
+//const dogBreed=["Border Collie","Pibble","Labrador","Papillon","Husky","Golden Retriever","Welsh Corgi"]
+let strings=[];
+
+function exerciseDog (dogName, dogBreed)
+{
+  for (let i=0; i<routine.length ; i++)
+  strings[i]=routine[i](dogName,dogBreed);
+  return(strings);
+}
+//console.log(exerciseDog(dogName,dogBreed));
