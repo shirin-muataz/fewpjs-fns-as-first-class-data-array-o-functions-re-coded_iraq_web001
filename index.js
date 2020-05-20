ac47f4b38e5381bc9c6dfd2569a963a9fe78d152
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-20 09:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const strings=[];
 function exerciseDog (dogName, dogBreed)
 {
   for (let i=0; i<routine.length ; i++)
-  strings=routine[i](dogName,dogBreed);
-  return(strings);
+  {strings=routine[i](dogName,dogBreed);
+  return(strings);}
 }
 console.log(exerciseDog(dogName,dogBreed));
